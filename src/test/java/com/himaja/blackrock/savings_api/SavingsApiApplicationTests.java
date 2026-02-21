89108be7c70e1425c4b481c0ef05120b837382fa
Himaja <himajareddy1729@gmail.com>
2026-02-21 11:04:34 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/himaja/blackrock/savings_api/SavingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himaja.blackrock.savings_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
